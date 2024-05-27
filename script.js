dd6a59b806d4c9dcc46cef77d1ea279c1968a68e
Martina Callegaro <marticallegaro29j@gmail.com>
2024-05-27 14:20:30 -0300

    se modifico el carrousel en html, css y javascript

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 //carrousel script.
+  const imagenes = [
+     'img/Carrousel/BIENVENIDOS.png',
+     'img/Carrousel/Tostado.png',
+     'img/Carrousel/CAFETERAS.png',
+     'img/Carrousel/coffe3.png'
+  ];
+
+  let posActual = 0;
   
+
+  const carrouselImagen = document.getElementById('carrouselImg');
+  const botonAnt = document.querySelector('prev');
+  const botonSig = document.querySelector('next');
+
+  function mostrarImagen(posActual){
+    carrouselImagen.src = imagenes(posActual);
+  }
+
+  function imagenAnterior(){
+    if (posicionActual <= 0) {
+        posicionActual = imagenes.length - 1;
+      } else {
+        posicionActual--;
+      }
+      mostrarImagen(posActual);
+    }
+  
+
+  function imagenSiguiente(){
+    if (posicionActual >= imagenes.length - 1) {
+        posicionActual = 0;
+      } else {
+        posicionActual++;
+      }
+      mostrarImagen(posActual);
+    }
+
+  botonAnt.addEventListener('click', () => {
+    imagenAnterior();
+  });  
+
+  botonSig.addEventListener('click', () => { 
+   imagenSiguiente();
+  }); 
+
+
+  mostrarImagen(Aindex);
+
+
