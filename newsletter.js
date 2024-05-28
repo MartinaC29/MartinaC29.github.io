25dab3ab548eb5ea85aeedf6c31ef7039190724c
Martina Callegaro <marticallegaro29j@gmail.com>
2024-05-28 01:20:07 -0300

    modificaciones

--- newsletter.js
+++ newsletter.js
@@ -1,21 +1,22 @@
-//validacion newsletter
 
-const name = document.getElementById(nombre);
-const lName = document.getElementById(apellido);
-const mail = document.getElementById(email);
-const form = document.getElementById(newsletter-form);
-const parrafo = document.getElementById(mensajes);
+const name = document.getElementById('nombre');
+const lName = document.getElementById('apellido');
+const mail = document.getElementById('email');
+const form = document.getElementById('newsletter-form');
+const parrafo = document.getElementById('mensajes');
 
 form.addEventListener("submit", e=>{
   e.preventDefault();
+  console.log("Formulario enviado");
   let mensajes = "";
   let ingresar = false;
   let regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/; 
-  parrafo.innerHTML = ""
+  parrafo.innerHTML = "";
 
   if(name.value.length < 3 || name.value.length > 30){
       mensajes += `El nombre no es valido <br>`
       ingresar = true
+      console.log("Nombre no válido");
   }
   if(lName.value.length < 3 || lName.value.length > 30){
       mensajes += `El apellido no es valido <br>`
@@ -29,8 +30,12 @@ form.addEventListener("submit", e=>{
   if(ingresar){
       parrafo.innerHTML = mensajes;
   }else{
-    parrafo.innerHTML = "Estas Suscrito"
+    parrafo.innerHTML = "Estas Suscrito";
+    name.value = "";
+    lName.value = "";
+    mail.value = "";
   }
+  
 
 
 });
