952faa506c57e8fbcc223a0ca2fd6bd617a0f384
Martina Callegaro <marticallegaro29j@gmail.com>
2024-06-07 17:10:48 -0300

    Cambios en formulario.
    Se corrigió el formulario de newsletter, se agrego otro javascript para el formulario y otro estilo para el newsletter.

--- newsletter.js
+++ newsletter.js
@@ -1,41 +1,97 @@
 
-const name = document.getElementById('nombre');
-const lName = document.getElementById('apellido');
-const mail = document.getElementById('email');
-const form = document.getElementById('newsletter-form');
-const parrafo = document.getElementById('mensajes');
+const newsletter = document.getElementById('newsletter-form');
+const inputs = document.querySelectorAll('#newsletter-form input');
+const expresiones = {
+    dni: /^.{4,8}$/,
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+    apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	telefono: /^\d{8,10}$/
+}
 
-form.addEventListener("submit", e=>{
-  e.preventDefault();
-  console.log("Formulario enviado");
-  let mensajes = "";
-  let ingresar = false;
-  let regexMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/; 
-  parrafo.innerHTML = "";
+const campos = {
+    dni: false,
+    nombre: false,
+    apellido: false,
+    correo: false,
+    telefono: false
 
-  if(name.value.length < 3 || name.value.length > 30){
-      mensajes += `El nombre no es valido <br>`
-      ingresar = true
-      console.log("Nombre no válido");
-  }
-  if(lName.value.length < 3 || lName.value.length > 30){
-      mensajes += `El apellido no es valido <br>`
-      ingresar = true
-  }
-  if(!regexMail.test(mail.value)){
-      mensajes += `El email no es valido <br>`
-      ingresar = true
-  }
+}
 
-  if(ingresar){
-      parrafo.innerHTML = mensajes;
-  }else{
-    parrafo.innerHTML = "Estas Suscrito";
-    name.value = "";
-    lName.value = "";
-    mail.value = "";
-  }
-  
+const validarNews = (e) => {
+    switch(e.target.name){
+        case "dni": 
+            validarCampo(expresiones.dni, e.target,'dni');
+        break;
+        case "nombre": 
+            validarCampo(expresiones.nombre, e.target,'nombre');
+        break;
+        case "apellido": 
+            validarCampo(expresiones.apellido, e.target,'apellido');
+        break;
+        case "mail": 
+            validarCampo(expresiones.correo, e.target,'mail');
+        break;
+        case "telefono": 
+            validarCampo(expresiones.telefono, e.target,'telefono');
+        break;   
+    } 
+   
+
+}
+
+const validarCampo = (expresion, input, campo) => {
+    if(expresion.test(input.value)){
+        document.getElementById(`g_${campo}`).classList.remove('g_form-incorrecto');
+        document.getElementById(`g_${campo}`).classList.add('g_form-correcto');
+        document.querySelector(`#g_${campo} i`).classList.remove('fa-circle-xmark');
+        document.querySelector(`#g_${campo} i`).classList.add('fa-circle-check');
+        document.querySelector(`#g_${campo} .error-form`).classList.remove('error_mensaje-activo');
+        campos[campo] = true;
+       }else{
+         document.getElementById(`g_${campo}`).classList.add('g_form-incorrecto');
+         document.getElementById(`g_${campo}`).classList.remove('g_form-correcto');
+         document.querySelector(`#g_${campo} i`).classList.remove('fa-circle-check');
+         document.querySelector(`#g_${campo} i`).classList.add('fa-circle-xmark');
+         document.querySelector(`#g_${campo} .error-form`).classList.add('error_mensaje-activo');
+         campos[campo] = false;
+       }
+
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener('keyup', validarNews);
+    input.addEventListener('blur', validarNews);
+
+});
+
+
+newsletter.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const terminos = document.getElementById('terminos');
+
+    if(campos.dni && campos.nombre && campos.apellido && campos.mail && campos.telefono && terminos.checked){
+
+        newsletter.reset();
+
+        document.getElementById('form_mensaje').classList.add('form_mensaje-activo');
+        setTimeout(() => {
+            document.getElementById('form_mensaje').classList.remove('form_mensaje-activo');
+
+
+        }, 5000);
+
+        document.querySelectorAll('.g_form-correcto').forEach((icono) => {
+            icono.classList.remove('g_form-correcto');
+        });
+
+        document.getElementById('error_mensaje').classList.remove('error_mensaje-activo');
+
+
+    }else{
+        document.getElementById('error_mensaje').classList.add('error_mensaje-activo');
+    }
 
 
 });
